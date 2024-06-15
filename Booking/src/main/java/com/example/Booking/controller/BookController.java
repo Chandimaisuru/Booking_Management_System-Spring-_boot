7a852d8df506df61444a97260bf98c0232cceca0
Chandimaisuru <isuruchandima92@gmail.com>
2024-06-15 10:02:18 +0530

    Backend done

--- Booking/src/main/java/com/example/Booking/controller/BookController.java
+++ Booking/src/main/java/com/example/Booking/controller/BookController.java
@@ -3,15 +3,15 @@ package com.example.Booking.controller;
 
 import com.example.Booking.dto.BookDto;
 import com.example.Booking.dto.ResponseDto;
+import com.example.Booking.entity.Book;
 import com.example.Booking.service.BookService;
 import com.example.Booking.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/book")
@@ -53,4 +53,103 @@ public class BookController {
             return new ResponseEntity(responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @PutMapping(value = "/updateBook")
+    public ResponseEntity updateBook(@RequestBody BookDto bookDto){
+        try {
+            String res = bookService.updateBook(bookDto);
+            if (res.equals("00")){
+                responseDto.setCode(VarList.RSP_SUCCESS);
+                responseDto.setMessage("Success");
+                responseDto.setContent(bookDto);
+                return new ResponseEntity(responseDto, HttpStatus.ACCEPTED);
+
+            }else if(res.equals("01")){
+
+                responseDto.setCode(VarList.RSP_DUPLICATED);
+                responseDto.setMessage("Not a register book");
+                responseDto.setContent(bookDto);
+                return new ResponseEntity(responseDto, HttpStatus.BAD_REQUEST);
+
+            }else {
+                responseDto.setCode(VarList.RSP_FAIL);
+                responseDto.setMessage("Error");
+                responseDto.setContent(null);
+                return new ResponseEntity(responseDto, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            responseDto.setCode(VarList.RSP_ERROR);
+            responseDto.setMessage(ex.getMessage());
+            responseDto.setContent(null);
+            return new ResponseEntity(responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/getAllBook")
+    public ResponseEntity getAllBook(){
+        try {
+            List<BookDto> bookDtoList = bookService.getAllBooks();
+            responseDto.setCode(VarList.RSP_SUCCESS);
+            responseDto.setMessage("Success");
+            responseDto.setContent(bookDtoList);
+            return new ResponseEntity(responseDto, HttpStatus.ACCEPTED);
+
+        }catch (Exception ex){
+            responseDto.setCode(VarList.RSP_ERROR);
+            responseDto.setMessage(ex.getMessage());
+            responseDto.setContent(null);
+            return new ResponseEntity(responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/searchBook/{bookID}")
+    public ResponseEntity searchBook(@PathVariable  int bookID){
+        try {
+            BookDto bookDto = bookService.searchBook(bookID);
+            if (bookDto != null){
+                responseDto.setCode(VarList.RSP_SUCCESS);
+                responseDto.setMessage("Success");
+                responseDto.setContent(bookDto);
+                return new ResponseEntity(responseDto, HttpStatus.ACCEPTED);
+
+
+            }else {
+                responseDto.setCode(VarList.RSP_FAIL);
+                responseDto.setMessage("No such book");
+                responseDto.setContent(null);
+                return new ResponseEntity(responseDto, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            responseDto.setCode(VarList.RSP_ERROR);
+            responseDto.setMessage(ex.getMessage());
+            responseDto.setContent(ex);
+            return new ResponseEntity(responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping (value = "/deleteBook/{bookID}")
+    public ResponseEntity deleteBook(@PathVariable  int bookID){
+        try {
+            String res= bookService.deleteBook(bookID);
+            if (res.equals("00")){
+                responseDto.setCode(VarList.RSP_SUCCESS);
+                responseDto.setMessage("Success");
+                responseDto.setContent(null);
+                return new ResponseEntity(responseDto, HttpStatus.ACCEPTED);
+
+
+            }else {
+                responseDto.setCode(VarList.RSP_FAIL);
+                responseDto.setMessage("No such book");
+                responseDto.setContent(null);
+                return new ResponseEntity(responseDto, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception ex){
+            responseDto.setCode(VarList.RSP_ERROR);
+            responseDto.setMessage(ex.getMessage());
+            responseDto.setContent(ex);
+            return new ResponseEntity(responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
--- Booking/src/main/java/com/example/Booking/service/BookService.java
+++ Booking/src/main/java/com/example/Booking/service/BookService.java
@@ -6,8 +6,12 @@ import com.example.Booking.repo.BookRepo;
 import com.example.Booking.util.VarList;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -31,4 +35,37 @@ public class BookService {
         }
 
     }
+    public String updateBook(BookDto bookDto) {
+        if(bookRepo.existsById(bookDto.getBookID())){
+            bookRepo.save(modelMapper.map(bookDto, Book.class));
+            return VarList.RSP_SUCCESS;
+        }else{
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
+
+    public List<BookDto> getAllBooks() {
+        List<Book> bookList = bookRepo.findAll();
+        return modelMapper.map(bookList,new TypeToken<ArrayList<BookDto>>(){}.getType());
+    }
+
+    public BookDto searchBook(int bookID){
+        if(bookRepo.existsById(bookID)){
+            Book book = bookRepo.findById(bookID).orElse(null);
+            return modelMapper.map(book ,BookDto.class);
+        }else{
+            return null;
+        }
+    }
+
+    public String deleteBook(int bookID){
+        if(bookRepo.existsById(bookID)){
+            bookRepo.deleteById(bookID);
+            return VarList.RSP_SUCCESS;
+        }else{
+            return VarList.RSP_NO_DATA_FOUND;
+        }
+    }
+
+
 }
